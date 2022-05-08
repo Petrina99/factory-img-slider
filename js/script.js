293ec023dd425cdf3981f33e7a0a6ea64df27741
Mislav Petrina <petrinamislav@gmail.com>
2022-05-08 19:16:43 +0200

    made it work, have to make it move with width of the last picture

--- js/script.js
+++ js/script.js
@@ -1,85 +1,56 @@
 $(document).ready(() => {
 
   $('.next').on('click', () => {
-    
-    let sliderRect = document.querySelector('.slider').getBoundingClientRect();
 
-    var firstImg = document.querySelector('.img1');
-    var secondImg = document.querySelector('.img2');
-    var thirdImg = document.querySelector('.img3');
-    var fourthImg = document.querySelector('.img4');
+    var firstImg = $('.img1');
+    var secondImg = $('.img2');
+    var thirdImg = $('.img3');
+    var fourthImg = $('.img4');
 
-    // coordinates of every image
-    let x1 = getX(firstImg, sliderRect);
-    let y1 = getY(firstImg, sliderRect);
+    let width = fourthImg.width();
 
-    let x2 = getX(secondImg, sliderRect);
-    let y2 = getY(secondImg, sliderRect);
+    console.log(width);
 
-    let x3 = getX(thirdImg, sliderRect);
-    let y3 = getY(thirdImg, sliderRect);
+    let positionFirst = firstImg.position();
+    let positionSecond = secondImg.position();
+    let positionThird = thirdImg.position();
+    let positionFourth = fourthImg.position();
 
-    let x4 = getX(fourthImg, sliderRect);
-    let y4 = getY(fourthImg, sliderRect);
+    firstImg.animate({
+      left: `+=${width}`
+    }, 200, "linear", () => {
+      firstImg.removeClass('img1').addClass('img2');
+    });
 
-    let width = getWidth(fourthImg);
-    
-    moveImage(firstImg, x1, y1, x2, y2);
-    moveImage(secondImg, x2, y2, x3, y3);
-    moveImage(thirdImg, x3, y3, x4, y4);
-    moveImage(fourthImg, x4, y3, x1, y1);
+    secondImg.animate({
+      left: `+=${width}`
+    }, 200, "linear", () => {
+      secondImg.removeClass('img2').addClass('img3');
+    });
 
-    firstImg.classList.remove('img1');
-    firstImg.classList.add('img2');
-    
-    secondImg.classList.remove('img2');
-    secondImg.classList.add('img3');
+    thirdImg.animate({
+      left: `+=${width}`
+    }, 200, "linear", () => {
+      thirdImg.removeClass('img3').addClass('img4');
+    });
 
-    thirdImg.classList.remove('img3');
-    thirdImg.classList.add('img4');
+    fourthImg.animate({
+      left: `+=${-width * 3}`
+    }, 200, "linear", () => {
+      fourthImg.removeClass('img4').addClass('img1');
+    });
 
-    fourthImg.classList.remove('img4');
-    fourthImg.classList.add('img1');
-
-    console.log("x1: " + x1 + " x2: " + x2 + " x3: " + x3 + " x4: " + x4);
   });
 });
 
-function getX(slide, slider) {
-  let slideRect = slide.getBoundingClientRect();
-
-  let offsetX = slideRect.left - slider.left;
-
-  return offsetX;
-}
-
-function getY(slide, slider) {
-  let slideRect = slide.getBoundingClientRect();
-
-  let offsetY = slideRect.top - slider.top;
-
-  return offsetY;
-}
-
-function getWidth(slide) {
-  let slideWidth = slide.getBoundingClientRect();
-
-  return slideWidth.width;
-}
-
 // slide we want to move, coordinates of the slide we want to move (x1, y1) and coordinates where it has to go
-function moveImage(slide, x1, y1, x2, y2) {
+/* function moveImage(slide, width, mutliplier) {
 
-  let x = x2 - x1;
-  let y = y2 - y1;
-
-  console.log(slide, x , y);
   anime({
     targets: slide,
-    translateX: x,
-    translateY: y,
-    duration: 1500,
-    easing: "easeOutExpo"
+    translateX: width * mutliplier,
+    duration: 500,
+    easing: "easeOutExpo",
   });
 
-}
+}*/
