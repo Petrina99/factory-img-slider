475ee430fdd2a727cf65817b20fed98e7b449bc1
Mislav Petrina <petrinamislav@gmail.com>
2022-05-08 18:21:50 +0200

    got the initial animation

--- js/script.js
+++ js/script.js
@@ -1,28 +1,85 @@
 $(document).ready(() => {
 
   $('.next').on('click', () => {
-
-    let firstImg = $('.img-one');
-    let secondImg = $('.img-two');
-    let thirdImg = $('.img-three');
-    let lastImg = $('.img-four');
-
-    firstImg.removeClass('img-one').addClass('img-two');
-    secondImg.removeClass('img-two').addClass('img-three');
-    thirdImg.removeClass('img-three').addClass('img-four');
-    lastImg.removeClass('img-four').addClass('img-one');
-  });
-
-  $('.prev').on('click', () => {
-
-    var firstImg = $('.img-one');
-    var secondImg = $('.img-two');
-    var thirdImg = $('.img-three');
-    var lastImg = $('.img-four');
     
-    firstImg.removeClass('img-one').addClass('img-four');
-    secondImg.removeClass('img-two').addClass('img-one');
-    thirdImg.removeClass('img-three').addClass('img-two');
-    lastImg.removeClass('img-four').addClass('img-three');
+    let sliderRect = document.querySelector('.slider').getBoundingClientRect();
+
+    var firstImg = document.querySelector('.img1');
+    var secondImg = document.querySelector('.img2');
+    var thirdImg = document.querySelector('.img3');
+    var fourthImg = document.querySelector('.img4');
+
+    // coordinates of every image
+    let x1 = getX(firstImg, sliderRect);
+    let y1 = getY(firstImg, sliderRect);
+
+    let x2 = getX(secondImg, sliderRect);
+    let y2 = getY(secondImg, sliderRect);
+
+    let x3 = getX(thirdImg, sliderRect);
+    let y3 = getY(thirdImg, sliderRect);
+
+    let x4 = getX(fourthImg, sliderRect);
+    let y4 = getY(fourthImg, sliderRect);
+
+    let width = getWidth(fourthImg);
+    
+    moveImage(firstImg, x1, y1, x2, y2);
+    moveImage(secondImg, x2, y2, x3, y3);
+    moveImage(thirdImg, x3, y3, x4, y4);
+    moveImage(fourthImg, x4, y3, x1, y1);
+
+    firstImg.classList.remove('img1');
+    firstImg.classList.add('img2');
+    
+    secondImg.classList.remove('img2');
+    secondImg.classList.add('img3');
+
+    thirdImg.classList.remove('img3');
+    thirdImg.classList.add('img4');
+
+    fourthImg.classList.remove('img4');
+    fourthImg.classList.add('img1');
+
+    console.log("x1: " + x1 + " x2: " + x2 + " x3: " + x3 + " x4: " + x4);
   });
 });
+
+function getX(slide, slider) {
+  let slideRect = slide.getBoundingClientRect();
+
+  let offsetX = slideRect.left - slider.left;
+
+  return offsetX;
+}
+
+function getY(slide, slider) {
+  let slideRect = slide.getBoundingClientRect();
+
+  let offsetY = slideRect.top - slider.top;
+
+  return offsetY;
+}
+
+function getWidth(slide) {
+  let slideWidth = slide.getBoundingClientRect();
+
+  return slideWidth.width;
+}
+
+// slide we want to move, coordinates of the slide we want to move (x1, y1) and coordinates where it has to go
+function moveImage(slide, x1, y1, x2, y2) {
+
+  let x = x2 - x1;
+  let y = y2 - y1;
+
+  console.log(slide, x , y);
+  anime({
+    targets: slide,
+    translateX: x,
+    translateY: y,
+    duration: 1500,
+    easing: "easeOutExpo"
+  });
+
+}
