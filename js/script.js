0e2d0ff001154482efd5cda31fa4d57d464632b3
Mislav Petrina <petrinamislav@gmail.com>
2022-05-11 17:55:54 +0200

    functionality done, need to finnish up css

--- js/script.js
+++ js/script.js
@@ -2,55 +2,117 @@ $(document).ready(() => {
 
   $('.next').on('click', () => {
 
-    var firstImg = $('.img1');
-    var secondImg = $('.img2');
-    var thirdImg = $('.img3');
-    var fourthImg = $('.img4');
+    // had to make for both sliders because of the difference in width
+    // first slider
+    const imgOne = $('.img1');
+    const imgTwo = $('.img2');
+    const imgThree = $('.img3');
+    const imgFour = $('.img4');
+    const imgFive = $('.img5');
+    const imgSix = $('.img6');
+    const imgSeven = $('.img7');
 
-    let width = fourthImg.width();
+    // determines how much the last one moves back
+    let totalWidthFirst = imgOne.width() + imgTwo.width() + imgThree.width() + imgFour.width() + imgFive.width() + imgSix.width();
 
-    console.log(width);
+    // how much all of them move forward
+    let movingWidthFirst = imgSeven.width();
 
-    let positionFirst = firstImg.position();
-    let positionSecond = secondImg.position();
-    let positionThird = thirdImg.position();
-    let positionFourth = fourthImg.position();
-
-    firstImg.animate({
-      left: `+=${width}`
-    }, 200, "linear", () => {
-      firstImg.removeClass('img1').addClass('img2');
+    imgOne.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgOne.removeClass('img1').addClass('img2');
     });
 
-    secondImg.animate({
-      left: `+=${width}`
-    }, 200, "linear", () => {
-      secondImg.removeClass('img2').addClass('img3');
+    imgTwo.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgTwo.removeClass('img2').addClass('img3');
     });
 
-    thirdImg.animate({
-      left: `+=${width}`
-    }, 200, "linear", () => {
-      thirdImg.removeClass('img3').addClass('img4');
+    imgThree.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgThree.removeClass('img3').addClass('img4');
     });
 
-    fourthImg.animate({
-      left: `+=${-width * 3}`
-    }, 200, "linear", () => {
-      fourthImg.removeClass('img4').addClass('img1');
+    imgFour.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgFour.removeClass('img4').addClass('img5');
     });
 
+    imgFive.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgFive.removeClass('img5').addClass('img6');
+    });
+
+    imgSix.animate({
+      left: `+=${movingWidthFirst}`
+    }, 400, "linear", () => {
+      imgSix.removeClass('img6').addClass('img7');
+    });
+
+    imgSeven.animate({
+      left: `-=${totalWidthFirst}`
+    }, 400, "linear", () => {
+      imgSeven.removeClass('img7').addClass('img1');
+    });
+
+    // second slider
+    const imgOne2 = $('.img21');
+    const imgTwo2 = $('.img22');
+    const imgThree2 = $('.img23');
+    const imgFour2 = $('.img24');
+    const imgFive2 = $('.img25');
+    const imgSix2 = $('.img26');
+    const imgSeven2 = $('.img27');
+
+    let totalWidthSecond = imgOne2.width() + imgTwo2.width() + imgThree2.width() + imgFour2.width() + imgFive2.width() + imgSix2.width();
+
+    let movingWidthSecond = imgSeven2.width();
+
+    imgOne2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgOne2.removeClass('img21').addClass('img22');
+    });
+
+    imgTwo2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgTwo2.removeClass('img22').addClass('img23');
+    });
+
+    imgThree2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgThree2.removeClass('img23').addClass('img24');
+    });
+
+    imgFour2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgFour2.removeClass('img24').addClass('img25');
+    });
+
+    imgFive2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgFive2.removeClass('img25').addClass('img26');
+    });
+
+    imgSix2.animate({
+      left: `+=${movingWidthSecond}`
+    }, 400, "linear", () => {
+      imgSix2.removeClass('img26').addClass('img27');
+    });
+
+    imgSeven2.animate({
+      left: `-=${totalWidthSecond}`
+    }, 400, "linear", () => {
+      imgSeven2.removeClass('img27').addClass('img21');
+    });
   });
 });
-
-// slide we want to move, coordinates of the slide we want to move (x1, y1) and coordinates where it has to go
-/* function moveImage(slide, width, mutliplier) {
-
-  anime({
-    targets: slide,
-    translateX: width * mutliplier,
-    duration: 500,
-    easing: "easeOutExpo",
-  });
-
-}*/
